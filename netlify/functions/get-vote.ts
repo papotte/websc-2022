e6182f4a00be59ace4d7c46835912d6c768f3d2c
Ross Bermudez <rbermudez@anaconda.com>
2022-09-01 16:07:44 +0200

    cors headers for cloudflare

--- netlify/functions/get-vote.ts
+++ netlify/functions/get-vote.ts
@@ -1,30 +1,31 @@
-import { Handler }  from "@netlify/functions";
-import {config} from "dotenv";
+import { Handler } from '@netlify/functions';
+import { config } from 'dotenv';
 
-config({ 
-  path: '.env' 
+config({
+    path: '.env',
 });
 const {
     DATABASE_URL,
-    SUPABASE_SERVICE_API_KEY
+    SUPABASE_SERVICE_API_KEY,
 } = process.env;
 
 // Connect to our database 
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
+
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
 export const handler: Handler = async () => {
     const { data } = await supabase
         .from('vote')
         .select('*');
-    
+
     console.log('DATA', data);
-    
+
     return {
         statusCode: 200,
         body: JSON.stringify(data),
         headers: {
             'Access-Control-Allow-Origin': '*',
-          }
-        };
-  };
+        },
+    };
+};
